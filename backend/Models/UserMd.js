e49189fde96656a7d99e880c5d4bb8868b907087
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-10-18 00:22:58 +0330

    auth controller added

--- backend/Models/UserMd.js
+++ backend/Models/UserMd.js
@@ -3,11 +3,6 @@ const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
     },
-    idCard: {
-        type: String,
-        unique: [true, 'idCard is already taken'],
-        match: [/^([0-9]){10}$/g, 'phone is already taken']
-    },
     phone: {
         type: String,
         match: [/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g, 'format isnt matched'],
